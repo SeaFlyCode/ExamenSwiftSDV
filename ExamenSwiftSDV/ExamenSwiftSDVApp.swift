ecff85d3b94eb469e8af851928001140e9feda66
matheovieilleville <matheo20055@gmail.com>
2026-02-20 09:01:58 +0100

    Initial Commit

---
+++ ExamenSwiftSDV/ExamenSwiftSDVApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenSwiftSDVApp.swift
+//  ExamenSwiftSDV
+//
+//  Created by Mathéo Vieilleville on 20/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExamenSwiftSDVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
